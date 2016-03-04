646d85b6f3da546b719662e9303112c72468307d
yhsj0919 <1130402124@qq.com>
2016-03-04 17:35:16 +0800

    替换了一个来自xutils的泛型获取类

--- app/src/main/java/xyz/yhsj/mvprodemo/presenter/MainActivity.java
+++ app/src/main/java/xyz/yhsj/mvprodemo/presenter/MainActivity.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import xyz.yhsj.mvpro.presenter.ActivityPresenterImpl;
+import xyz.yhsj.mvpro.presenter.AppCompatActivityPresenterImpl;
 import xyz.yhsj.mvprodemo.R;
-import xyz.yhsj.mvprodemo.model.impl.OnLoadDataListener;
 import xyz.yhsj.mvprodemo.model.AppData;
 import xyz.yhsj.mvprodemo.model.empty.User;
 import xyz.yhsj.mvprodemo.model.impl.DataImpl;
+import xyz.yhsj.mvprodemo.model.impl.OnLoadDataListener;
 import xyz.yhsj.mvprodemo.view.MainView;
 
-public class MainActivity extends ActivityPresenterImpl<MainView> implements View.OnClickListener, OnLoadDataListener {
+public class MainActivity extends AppCompatActivityPresenterImpl<MainView> implements View.OnClickListener {
     private DataImpl dataImpl;
 
     @Override
@@ -25,18 +25,19 @@ public class MainActivity extends ActivityPresenterImpl<MainView> implements Vie
     public void onClick(View v) {
 
         if (v.getId() == R.id.fab) {
-            dataImpl.getUser(this);
+            dataImpl.getUser(new OnLoadDataListener<User>() {
+                @Override
+                public void onSuccess(User user) {
+                    Toast.makeText(MainActivity.this, user.getName(), Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onError() {
+                    Toast.makeText(MainActivity.this, "请求失败", Toast.LENGTH_LONG).show();
+                }
+            });
         }
 
     }
 
-    @Override
-    public void onSuccess(User user) {
-        Toast.makeText(this, user.getName(), Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onError() {
-        Toast.makeText(this, "请求失败", Toast.LENGTH_LONG).show();
-    }
 }
